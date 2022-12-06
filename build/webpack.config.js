91f232fb1b666a336430ec581503a83a75f03a08
libing-cheer <libing_cheer@163.com>
2022-12-06 23:50:00 +0800

    feat：webpack.config.js配置

---
+++ build/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: path.resolve(__dirname, '../src/index.js'),
+        header: path.resolve(__dirname, '../src/header.js')
+    },
+    output: {
+        filename: '[name].[fullhash:8].js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public/index.html'),
+            filename: 'index.html',
+            chunks: ['index']
+        }),
+
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public/header.html'),
+            filename: 'header.html',
+            chunks: ['header']
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'] // 从右向左解析原则
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            }
+        ]
+    },
+    mode: 'development'
+};
