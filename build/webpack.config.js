d39fbb1e92342b343f066553a2de9288b6cd4e1f
libing-cheer <libing_cheer@163.com>
2022-12-07 11:11:33 +0800

    feat：url-loader、file-loader图片、媒体文件、字体打包配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // html模版引擎
+const {CleanWebpackPlugin} = require('clean-webpack-plugin'); // 清除文件
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 打包css
+
+
 module.exports = {
     mode: 'development',
     entry: {
@@ -34,19 +36,61 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader'
-                ] // 从右向左解析原则
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // // 从右向左解析原则
             },
             {
                 test: /\.less$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'less-loader']
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/i, // 图片文件
                 use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'less-loader'
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'img/[name].[hash:8].[ext]'
+                                }
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(mp4|webm|mp3|wav|flac|acc)(\?.*)?$/, // 媒体文件
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'media/[name].[hash:8].[ext]'
+                                }
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff2?|eot|ttf)$(\?.*)?$/i, // 字体
+                use:[
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'fonts/[name].[hash:8].[ext]'
+                                }
+                            }
+                        }
+                    }
                 ]
             }
         ]
