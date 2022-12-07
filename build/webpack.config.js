58311a7694c486779f4d5947dea6cea0e887484f
libing-cheer <libing_cheer@163.com>
2022-12-07 09:42:47 +0800

    feat：为css添加浏览器前缀postcss

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
             },
             {
                 test: /\.less$/,
-                use: ['style-loader', 'css-loader', 'less-loader']
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader']
             }
         ]
     },
